ec0b0f31ecb7620508436488ed86792a02283f9b
Horst Gutmann <zerok@zerokspot.com>
2019-07-07 14:48:14 +0200

    Add auto-minor-mode setting

--- k8s-secret-mode.el
+++ k8s-secret-mode.el
@@ -5,18 +5,27 @@
 ;;; Author: Horst Gutmann <zerok@zerokspot.com>
 
 ;;; Commentary:
+;; This package defines a new minor-mode which adds some encoding and
+;; decoding functionality for Kubernetes Secret resources.
 ;;
+;; Right now, this package requires also the installation of a custom
+;; binary.  You can find details about the setup process on
+;; https://github.com/zerok/k8s-secret-mode.el.
 
 ;;; Code:
-(define-minor-mode k8s-secret-mode
-  "Toggle k8s-secret-mode."
-  nil
-  " k8s-secret")
+(require 'auto-minor-mode)
 
 (setq k8s-secret-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "C-c C-c") 'k8s-secret/toggle)
     map))
+
+(define-minor-mode k8s-secret-mode
+  "Toggle k8s-secret-mode."
+  :keymap k8s-secret-mode-map
+  :lighter " k8s-secret"
+  (when k8s-secret-mode
+    (setq k8s-secret--status nil)))
 
 (defvar k8s-secret--status nil)
 (make-variable-buffer-local 'k8s-secret--status)
@@ -25,6 +34,44 @@
   "*Hook run by `k8s-secret-mode'."
   :type 'hook
 :group 'k8s-secret)
+
+(defun k8s-secret-mode-magic-matches ()
+  (interactive)
+  (save-excursion
+    (progn
+      ; This function is usually triggered inside a narrowed
+      ; context. We need to widen it as the kind of a YAML file might
+      ; be after the data.
+      (widen)
+      (let ((matches nil))
+        (goto-char (point-min))
+        (let ((curline (line-number-at-pos))
+              (prevline -1))
+      
+          (while (not (or matches (equalp curline prevline)))
+            (setq prevline curline)
+            (forward-line)
+            (let* ((start (progn
+                            (beginning-of-line)
+                            (point)))
+                   (end (progn
+                          (end-of-line)
+                          (point)))
+                   (line (buffer-substring start end)))
+              (when (equalp line "kind: Secret")
+                (setq matches t)))
+            (setq curline (line-number-at-pos)))
+          )
+        (when (called-interactively-p 'any)
+          (message "Matches secret: %s" matches))
+        ; Restore narrowing for all other magic checks:
+        (narrow-to-region (point-min)
+                          (min (point-max)
+                               (+ (point-min) magic-mode-regexp-match-limit)))
+        matches))))
+      
+;; (add-to-list 'auto-minor-mode-magic-alist '(k8s-secret-mode-magic-matches . k8s-secret-mode))
+(setq auto-minor-mode-magic-alist (list '(k8s-secret-mode-magic-matches . k8s-secret-mode)))
 
 (defcustom k8s-secret-codec-bin (expand-file-name "~/src/github.com/zerok/k8s-secret-mode.el/bin/k8s-secret-codec")
   "*Path to the `k8s-secret-codec` binary."
